b78ebbfea91157b7b204c22a9cbaf347497c98c7
pedro martingaste <pitermartingaste@gmail.com>
2026-02-10 12:57:54 -0300

    new video

--- src/app/3dworks/page.js
+++ src/app/3dworks/page.js
@@ -351,6 +351,31 @@ export default function Works3DPage() {
                 }}
               ></iframe>
             </div>
+            <div style={{
+              position: 'relative',
+              width: '100%',
+              paddingBottom: '56.25%', // 16:9 aspect ratio
+              height: 0,
+              overflow: 'hidden'
+            }}>
+              <iframe 
+                src="https://www.youtube.com/embed/ujHB7Rc9sdA?si=U0Mw8w22nBNGGpNx" 
+                title="YouTube video player" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '8px',
+                  boxShadow: '0 4px 20px rgba(0, 0, 0, 0.5)'
+                }}
+              ></iframe>
+            </div>
           </div>
 
           <div style={{
